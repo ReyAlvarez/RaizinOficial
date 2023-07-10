ca318b73883f4a12fe0f3f758e7733e9aeba89de
Nacho Rey Alvarez <nacho.rei@gmail.com>
2023-07-09 22:55:17 -0300

    second commit

--- cart.js
+++ cart.js
@@ -1,23 +1,28 @@
+import { productList } from "./json/productsRaizin.js";
+
 const saveCart = () => {
-  if (productosCart.length > 0) {
-    localStorage.setItem("productosFav", JSON.stringify(productosFav));
+  if (productsCart.length > 0) {
+    localStorage.setItem("productsCart", JSON.stringify(productsCart));
   }
 };
 
-const recuperarFavoritos = () => {
-  return JSON.parse(localStorage.getItem("productosFav")) || [];
+const retrieveCart = () => {
+  return JSON.parse(localStorage.getItem("productsCart")) || [];
 };
 
-const productsCart = recuperarFavoritos();
+const productsCart = retrieveCart();
 
 const addToCart = (productId) => {
   if (productId > 0) {
-    const result = productsRaizin.find((product) => product.id === parseInt(productId));
-    console.log(result);
+    const result = productList.find((product) => product.id === parseInt(productId));
     if (result !== undefined) {
       productsCart.push(result);
+      saveCart();
+      document.getElementById("message").innerHTML = `Product ${productId} added to cart`;
     } else {
       document.getElementById("message").innerHTML = "I couldn't match and product to that ID";
     }
   }
 };
+
+addToCart();
--- mainScript.js
+++ mainScript.js
@@ -6,14 +6,40 @@ const dynamicCards = (product) => {
                 <div class="product_image "><img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src=${product.image}></div>
                 <div class="product_title"><p>${product.title}</p></div>
                 <div class="product_price"><p><b>$${product.price}</b></p></div>
-                <div class="product_description pb"><p>${product.description}</p>
-                <div class="add"><button class="button-addToCart flex justify-center pt-3 " id="${product.id}">Agregar al carrito</button></div>
+                <div class="product_description pb"><p>${product.description}</p></div>
+                <div class="add"><button class="button-addToCart btn-outline-dark bg-red-100  flex justify-center pt-3" id="${product.id}">Agregar al carrito 🛒</button></div>
           </div>`;
 };
-console.log(dynamicCards);
-function loadProducts() {
-  productList.forEach((product) => {
-    container.innerHTML += dynamicCards(product);
-  });
-}
-loadProducts();
+
+const addClickToCartBTN = () => {
+  const buttons = document.querySelectorAll("button.button-addToCart");
+  for (const button of buttons) {
+    button.addEventListener("click", (e) => {
+      addToCart(e.target.id);
+      saveCart();
+      showMessage("El producto se agrego correctamente al carrito 😃");
+    });
+  }
+};
+
+const showMessage = (message) => {
+  const msg = document.querySelector(".class-alertas");
+  msg.textContent = message;
+  setTimeout(() => {
+    msg.textContent = "";
+  }, 3000);
+};
+
+const loadProducts = (array) => {
+  container.innerHTML = "";
+  if (array.length > 0) {
+    array.forEach((product) => {
+      container.innerHTML += dynamicCards(product);
+    });
+    addClickToCartBTN();
+  } else {
+    container.innerHTML = "No hay productos disponibles";
+  }
+};
+
+loadProducts(productList);
